30583a3ecfba78dc929237789a585eeb757e6a8b
Carolina E. Zhagnay <carolinazhagnay01@gmail.com>
2019-04-03 22:31:34 +0000

    doneish

--- js/game.js
+++ js/game.js
@@ -1,16 +1,16 @@
 // NOTE - You must have a level with the name "start".  This is used as the first level in the game.
 
 var game = {
-    music: "98_Lost_Mine.mp3",
-    background_image: "intro-bg.jpg",
+    music: "opening.mp3",
+    background_image: "begin-back.jpg",
     levels: {
 
         start: {
             message: "You come across a beautiful butterfly",
             choices: [
                 {
-                    text: "You don't follow it",
-                    nextLevel: "field",
+                    text: "You don't follow it and go back home",
+                    nextLevel: "home",
                 },
                 {
                     text:"You follow it into a forest",
@@ -18,29 +18,88 @@ var game = {
                 }
             ]
         },
-
         forest: {
-            background_image: "fire.gif",
-            music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "A rabbit appears!",
+            music: "",
+            background_image: "rab-img.jpeg",
+            message: "You see rabbits appear!",
             choices: [
                 {
-                    text: "You follow the rabbit",
-                    nextLevel: "more",
+                    text: "You follow the rabbits",
+                    nextLevel: "deeper",
                 },
+                
+                {
+                    text:"You don't follow the rabbits and go back home",
+                    nextLevel:"home",
+                }
             ]
         },
 
-        field: {
-            message: "You go home",
+        deeper: {
+            music: "waterfall.mp3",
+            background_image: "water.gif",
+            message: "They hop along a stream of water...",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "You decide to abandon your shoes and follow the cute rabbit",
+                    nextLevel: "water",
                 },
+                {
+                    text: "You decide to not getting your brand new shoes dirty",
+                    nextLevel:"home"
+                }
             ]
         },
-
-    }
+        water: {
+            background_image: "paths.jpg",
+            message: "You've made it across but end up in front of two paths..",
+            choices: [
+                {
+                    text: "You decide to go to the left",
+                    nextLevel:"flowers",
+                },
+                {
+                    text: "You decide to go to the right",
+                    nextLevel:"death",
+                },
+                {
+                    text:"You just want to go home now", 
+                    nextLevel:"home",
+             }
+         ]
+    },
+        flowers: {
+            music: "goodEnd.mp3",
+            background_image: "field.jpg",
+            message: "You've come across a beautiful field of flowers. You decide to pick some and take them for your mom.",
+            choices: [
+                {
+                    text:"Go back home",
+                    nextLevel:"start"
+                }
+            ]
+        },
+        death: {
+            music: "death.mp3",
+            background_image: "badEnd.jpg",
+            message:"You fall into a hole never to be seen again...",
+            choices: [
+                {
+                    text: "You have died...now you start your life over...",
+                    nextLevel:"start",
+                }
+            ]
+        },
+        home: {
+            background_image: "homePage.jpeg",
+            message:"Well, now you'll never know how interesting the forest can be...",
+            choices: [
+                {
+                    text: "Start Over",
+                    nextLevel:"start",
+                }
+            ]
+        }
+    },
 };
        
